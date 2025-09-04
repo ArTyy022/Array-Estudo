21ebfb49240a39c235568d6f01f87172e5b9e42a
ArTyy022 <toledoarthur11@gmail.com>
2025-09-04 11:39:58 -0300

    Add files via upload

--- Array alura Módulo 3/src/AtividadeQuatro/Produto.java
+++ Array alura Módulo 3/src/AtividadeQuatro/Produto.java
@@ -3,8 +3,8 @@ package AtividadeQuatro;
 import java.util.ArrayList;
 
 public class Produto {
-     String nome;
-     double preco;
+     String nome = "Arroz";
+     double preco = 8.75;
      ArrayList<Produto> listaDeProdutos = new ArrayList<>();
-     
+
 }
