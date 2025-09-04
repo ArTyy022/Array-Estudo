6b79ea807b61bf682658f9f5209568bf478c620b
ArTyy022 <toledoarthur11@gmail.com>
2025-09-04 20:52:34 -0300

    Add files via upload

---
+++ Array alura Módulo 4/src/AtividadeDois/Titulo.java
@@ -0,0 +1,13 @@
+package AtividadeDois;
+
+
+
+public class Titulo implements Comparable<Titulo> {
+    String nome;
+
+
+    @Override
+    public int compareTo(Titulo outroTitulo) {
+        return this.nome.compareTo(outroTitulo.nome);
+    }
+}
