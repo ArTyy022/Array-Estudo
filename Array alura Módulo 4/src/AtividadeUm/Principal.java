803dbfdd187b039aec1d7675db28b7985e9091a4
ArTyy022 <toledoarthur11@gmail.com>
2025-09-04 20:48:27 -0300

    Add files via upload

--- Array alura Módulo 4/src/AtividadeUm/Principal.java
+++ Array alura Módulo 4/src/AtividadeUm/Principal.java
@@ -1,4 +1,27 @@
 package AtividadeUm;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Principal {
+    public static void main(String[] args) {
+
+        int numero1 = 1;
+        int numero2 = 2;
+        int numero3 = 3;
+        int numero4 = 4;
+
+        ArrayList<Integer> lista = new ArrayList<>();
+        lista.add(numero4);
+        lista.add(numero3);
+        lista.add(numero2);
+        lista.add(numero1);
+        Collections.sort(lista);
+        System.out.println(lista);
+
+
+
+
+    }
+
 }
