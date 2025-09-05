d48e3f94e5f30b424352cd6f5d2da60c079a9226
ArTyy022 <toledoarthur11@gmail.com>
2025-09-05 18:58:05 -0300

    Add files via upload

---
+++ Array alura Módulo 4/src/AtividadeQuatro/Secundario.java
@@ -0,0 +1,21 @@
+package AtividadeQuatro;
+
+public class Secundario {
+    String nome;
+
+    public Secundario(String nome){
+        this.nome = nome;
+
+
+
+        }
+
+    @Override
+    public String toString() {
+        return nome;
+    }
+}
+
+
+
+
